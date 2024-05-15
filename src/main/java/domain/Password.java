565c2b1d67dabdf31e71edead21c3b001bdb91f6
Jakub9115 <wac220786@spengergasse.at>
2024-05-15 19:12:41 +0200

    added bcrypt, spring-security-crypto and zxcvbn library; added a Method to Password class in uml.text; and completed Class Password.java

--- src/main/java/domain/Password.java
+++ src/main/java/domain/Password.java
@@ -1,14 +1,35 @@
 package domain;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import com.nulabinc.zxcvbn.Strength;
+import com.nulabinc.zxcvbn.Zxcvbn;
+
 public class Password {
 
+    // max 100 characters
     private char[] hashedPassword;
 
-    public char[] hashPassword(String password){
-        return null;
+    public boolean isPasswordStrong(String password){
+        Zxcvbn zxcvbn = new Zxcvbn();
+        Strength strength = zxcvbn.measure(password);
+        return strength.getScore() >= 3;
     }
 
-    public boolean checkPasswords(String password, String password2){
-        return false;
+    public char[] hashPassword(String password){
+
+        hashedPassword = BCrypt.withDefaults().hashToChar(12, password.toCharArray());
+        return hashedPassword;
+    }
+
+    public boolean checkPasswords(String password, char[] hashedPassword){
+        return BCrypt.verifyer().verify(password.toCharArray(), hashedPassword).verified;
+    }
+
+    public static void main(String[] args) {
+        Password password = new Password();
+
+        System.out.println(password.checkPasswords("cool", password.hashPassword("cool")));
+
+        password.isPasswordStrong("cool");
     }
 }
