758ce7d8b5eb822deaa0c8a3582a9579c261a401
Patrick9107 <POP220780@spengergasse.at>
2024-05-15 16:38:13 +0200

    import Assert to User and created hashcode + equals in BaseEntity

--- src/main/java/domain/User.java
+++ src/main/java/domain/User.java
@@ -3,6 +3,8 @@ package domain;
 import java.util.List;
 import java.util.Set;
 
+import static foundation.Assert.*;
+
 public class User extends BaseEntity {
     private Email email;
     private Password hashedPassword;
@@ -12,8 +14,7 @@ public class User extends BaseEntity {
     private List<Chat> chats;
     private Set<Medicalcase> partOfMedicalcase; // Diese Variable vllt noch ändern weil man ja owner oder member sein kann. Also eventuell eine Map + der name der Variable ist auch nicht schön
 
-    public void verify(){
-
+    public void verify() {
     }
 
     public Email getEmail() {
--- src/test/java/domain/UserTest.java
+++ src/test/java/domain/UserTest.java
@@ -1,7 +1,13 @@
 package domain;
 
+import org.junit.jupiter.api.BeforeEach;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class UserTest {
 
+    @BeforeEach
+    void setUp() {
+
+    }
 }
