85a4ceb7665abe779e5a2cf329cfe34b5a43baf8
Patrick9107 <POP220780@spengergasse.at>
2024-05-15 21:17:18 +0200

    change String to char[] in Password

--- src/main/java/domain/User/Password.java
+++ src/main/java/domain/User/Password.java
@@ -4,20 +4,31 @@ import at.favre.lib.crypto.bcrypt.BCrypt;
 import com.nulabinc.zxcvbn.Strength;
 import com.nulabinc.zxcvbn.Zxcvbn;
 
+import java.util.Arrays;
+
 public class Password {
 
     // max 100 characters
     private char[] hashedPassword;
 
-    public boolean isPasswordStrong(String password){
+    public Password(char[] rawPassword) {
+        setHashedPassword(rawPassword);
+    }
+
+    public void setHashedPassword(char[] rawPassword) {
+        this.hashedPassword = hashPassword(rawPassword);
+    }
+
+    public boolean isPasswordStrong(CharSequence password) { // we have to look into CharSequence
+        // found that CharSequence is not necessarily immutable (which means its internal state, unlike Strings, can change) which makes it a good candidate in this case for the password to not have anyone catch traces of it in the heap space
         Zxcvbn zxcvbn = new Zxcvbn();
         Strength strength = zxcvbn.measure(password);
         return strength.getScore() >= 3;
     }
 
-    public char[] hashPassword(String password){
+    public char[] hashPassword(char[] password){
 
-        hashedPassword = BCrypt.withDefaults().hashToChar(12, password.toCharArray());
+        hashedPassword = BCrypt.withDefaults().hashToChar(12, password);
         return hashedPassword;
     }
 
