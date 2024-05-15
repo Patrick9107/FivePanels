2176fad98d16989ccbc197d31eb8a29b3b3a85b7
Jakub9115 <wac220786@spengergasse.at>
2024-05-15 17:48:25 +0200

    removed Hashtag and added the Hashtag method to Spezialisation.java and CaseTage.java

--- src/main/java/domain/CaseTag.java
+++ src/main/java/domain/CaseTag.java
@@ -1,10 +1,36 @@
 package domain;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
-public abstract class CaseTag extends Hashtag {
-//    static {
-//        tags = new HashSet<>();
-//        readTagsFromFile("src/main/resources/casetags.txt");
-//    }
+public abstract class CaseTag {
+
+    public static Set<String> caseTag;
+
+    public static Set<String> caseTags() {
+
+        return caseTag;
+    }
+
+    public static void readTagsFromFile(String filename) {
+
+        try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
+
+            String line;
+            while ((line = br.readLine()) != null) {
+
+                caseTag.add(line);
+            }
+        } catch (FileNotFoundException e) {
+            throw new MedicalcaseException(STR."\{e.getMessage()}: file was not found");
+        } catch (IOException e) {
+            throw new MedicalcaseException(STR."\{e.getMessage()}: error reading file");
+        }
+    }
+
 }
--- src/main/java/domain/Specialization.java
+++ src/main/java/domain/Specialization.java
@@ -1,11 +1,33 @@
 package domain;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.HashSet;
+import java.util.Set;
 
-public abstract class Specialization extends Hashtag {
+public abstract class Specialization {
+    public static Set<String> specializations;
 
-    static {
-        tags = new HashSet<>();
-        readTagsFromFile("src/main/resources/specializations.txt");
+    public static Set<String> getSpecializations() {
+
+        return specializations;
+    }
+
+    public static void readSpecializationsFromFile(String filename) {
+
+        try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
+
+            String line;
+            while ((line = br.readLine()) != null) {
+
+                specializations.add(line);
+            }
+        } catch (FileNotFoundException e) {
+            throw new MedicalcaseException(STR."\{e.getMessage()}: file was not found");
+        } catch (IOException e) {
+            throw new MedicalcaseException(STR."\{e.getMessage()}: error reading file");
+        }
     }
 }
