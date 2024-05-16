acc2c8a9761de68b2020a7b45f414975eacca5a1
Patrick Poppinger <9107@htl.rennweg.at>
2024-05-17 00:54:59 +0200

    add EmailValidator

--- src/main/java/domain/User/Email.java
+++ src/main/java/domain/User/Email.java
@@ -1,7 +1,22 @@
 package domain.User;
 
+import org.apache.commons.validator.routines.EmailValidator;
+
+import static foundation.Assert.*;
+
 public class Email {
 
     // not null, max 64 characters before "@", max 255 characters after "@", min 3 characters including the "@", has to include "@"
     private String address;
+
+    public Email(String address) {
+        setAddress(address);
+    }
+
+    public void setAddress(String address) {
+
+        if (!(EmailValidator.getInstance().isValid(address)))
+            throw new UserException("setAddress(): address is not valid");
+        this.address = address;
+    }
 }
