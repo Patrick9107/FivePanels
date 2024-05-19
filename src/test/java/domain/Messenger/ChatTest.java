b887acedb35d35e5e7e49ffac0b480961931f6fe
Patrick Poppinger <9107@htl.rennweg.at>
2024-05-20 01:09:49 +0200

    create test users in ChatTest

--- src/test/java/domain/Messenger/ChatTest.java
+++ src/test/java/domain/Messenger/ChatTest.java
@@ -1,5 +1,24 @@
 package domain.Messenger;
 
+import domain.User.User;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class ChatTest {
 
+    private User homer;
+    private User bart;
+
+    @BeforeEach
+    void setup() {
+        homer = new User("homer@simpson.com", "password", "Homer Simpson", "Rh.D.", "United Kingdom");
+        bart = new User("bart@simpson.com", "password", "Bart Simpson", "Ph.D.", "United States");
+    }
+
+    @Test
+    void test() {
+
+    }
 }
