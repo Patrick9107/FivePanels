ac8bb665148b0b97bf56a5a6fd1cb331bfb1ec9d
Patrick9107 <POP220780@spengergasse.at>
2024-06-05 20:33:55 +0200

    fixed Password authentication

--- src/main/java/domain/User/Password.java
+++ src/main/java/domain/User/Password.java
@@ -27,6 +27,7 @@ public class Password {
                 throw new UserException("setHashedPassword(): Password is not strong enough");
             }
         } finally {
+            // finally block is not neccessary but if an exception occurs during isPasswordStrong it is coming in handy
             for (int i = 0; i < rawPassword.length; i++) {
                 rawPassword[i] = 0;
             }
@@ -38,15 +39,9 @@ public class Password {
     }
 
     public boolean isPasswordStrong(char[] password) {
-        try {
             Zxcvbn zxcvbn = new Zxcvbn();
             Strength strength = zxcvbn.measure(java.nio.CharBuffer.wrap(password));
             return strength.getScore() >= 3;
-        } finally {
-            for (int i = 0; i < password.length; i++) {
-                password[i] = 0;
-            }
-        }
     }
 
     public char[] hashPassword(char[] password) {
